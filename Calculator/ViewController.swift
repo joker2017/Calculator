26206586d96ece3e87f0406318cc7dc5d2647bb4
Щербина Андрей <depsemt2009@MacBook-Pro-Serbina.local>
2017-01-01 19:38:00 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Щербина Андрей on 01.01.17.
+//  Copyright © 2017 Щербина Андрей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
